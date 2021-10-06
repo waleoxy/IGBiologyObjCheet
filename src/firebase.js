d5b67a988cdc7ba147698d1104806428e50d787d
Olawale Otubu <waleoxy@gmail.com>
2021-10-06 11:35:43 +0100

    initial commit, firebase setup and installations

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0CbINnoVMe4LjicDmp5f13Um3j_rps2U",
+  authDomain: "biologyquiz-aed65.firebaseapp.com",
+  projectId: "biologyquiz-aed65",
+  storageBucket: "biologyquiz-aed65.appspot.com",
+  messagingSenderId: "675410463442",
+  appId: "1:675410463442:web:c363cba13720a57922bb0b",
+  measurementId: "G-6ZJNNT8GLG"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
